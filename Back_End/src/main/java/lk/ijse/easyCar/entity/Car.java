fa6f1070aed2f3140167c689f26562787de5b78f
kavindu1thiwanka <kavidu12thiwanka@gmail.com>
2023-02-19 23:37:11 +0530

    car db

--- Back_End/src/main/java/lk/ijse/easyCar/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/easyCar/entity/Car.java
@@ -1,5 +1,27 @@
 package lk.ijse.easyCar.entity;
 
-public class Car {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Car {
+    @Id
+    private String registrationNum;
+    private String brand;
+    private String carType;
+    private int passengers;
+    private String fuelType;
+    private double dailyPrice;
+    private double monthlyPrice;
+    private int extraKM;
+    private String color;
 }
