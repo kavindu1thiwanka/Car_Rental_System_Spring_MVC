ea77eacad1a1cd3441145480b7ed5e82a1d000b2
kavindu1thiwanka <kavidu12thiwanka@gmail.com>
2023-02-19 13:18:13 +0530

    back end

---
+++ Back_End/src/main/java/lk/ijse/easyCar/controller/indexController.java
@@ -0,0 +1,11 @@
+package lk.ijse.easyCar.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/easy")
+public class indexController {
+}
