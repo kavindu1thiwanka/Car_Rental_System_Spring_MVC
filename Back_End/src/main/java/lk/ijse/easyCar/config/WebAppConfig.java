6af1c638dd3550999b1d108ba42355f72a77a22c
kavindu1thiwanka <kavidu12thiwanka@gmail.com>
2023-02-24 17:00:04 +0530

    coomit

--- Back_End/src/main/java/lk/ijse/easyCar/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/easyCar/config/WebAppConfig.java
@@ -1,16 +1,14 @@
 package lk.ijse.easyCar.config;
 
 import lk.ijse.easyCar.advisor.AppWideExceptionHandler;
-import lk.ijse.easyCar.controller.AdminController;
-import lk.ijse.easyCar.controller.HomePageController;
-import lk.ijse.easyCar.controller.RegisterController;
+import lk.ijse.easyCar.controller.*;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {AdminController.class,HomePageController.class, RegisterController.class,AppWideExceptionHandler.class})
+@ComponentScan(basePackageClasses = {AdminController.class,HomePageController.class, RegisterController.class, DriverController.class, UserController.class,VehicleController.class,AppWideExceptionHandler.class})
 public class WebAppConfig {
 
 
